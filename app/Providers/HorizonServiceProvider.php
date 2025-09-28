2e7511010a2e5538278afb605773604e26e78073
Erick Mwamodo <erick@mwamodo.com>
2025-09-28 21:36:47 +0300

    ADD: Integrate Laravel Horizon for enhanced queue management
    - Updated .env.example to switch queue connection from database to Redis.
    - Added Laravel Horizon as a new dependency in composer.json for better queue monitoring and management.
    - Created HorizonServiceProvider to configure Horizon settings and permissions.
    - Introduced a new configuration file for Horizon to define its behavior and settings.

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+    }
+
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function ($user = null) {
+            return in_array(optional($user)->email, [
+                //
+            ]);
+        });
+    }
+}
